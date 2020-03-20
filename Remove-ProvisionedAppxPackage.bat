a0eb0dea34cc8fdd109110c601c19c698876171b
Mohamed <39871067+hassmoha@users.noreply.github.com>
2020-03-20 10:37:16 -0500

    Add files via upload

---
+++ Remove-ProvisionedAppxPackage.bat
@@ -0,0 +1,24 @@
+@echo remove Remove-ProvisionedAppxPackages from your windows installation
+@rem use this command to the get all the list in your windows 10
+dism /online /Get-ProvisionedAppxPackages
+dism /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.BingWeather_4.36.20503.0_neutral_~_8wekyb3d8bbwe
+dism /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.GetHelp_10.1912.30071.0_neutral_~_8wekyb3d8bbwe
+dism /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.Getstarted_8.2.22942.0_neutral_~_8wekyb3d8bbwe
+dism /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.HEIFImageExtension_1.0.30722.0_x64__8wekyb3d8bbwe
+dism /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.Messaging_2019.125.32.1000_neutral_~_8wekyb3d8bbwe
+dism /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.MicrosoftSolitaireCollection_4.6.1224.0_neutral_~_8wekyb3d8bbwe
+dism /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.MixedReality.Portal_2000.20011.1312.0_neutral_~_8wekyb3d8bbwe
+dism /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.Wallet_2.4.18324.0_neutral_~_8wekyb3d8bbwe
+dism /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.WindowsFeedbackHub_2019.1111.2029.0_neutral_~_8wekyb3d8bbwe
+dism /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.WindowsMaps_2019.1008.1857.0_neutral_~_8wekyb3d8bbwe
+dism /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.WindowsSoundRecorder_2020.226.628.0_neutral_~_8wekyb3d8bbwe
+dism /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.Xbox.TCUI_1.24.10001.0_neutral_~_8wekyb3d8bbwe
+dism /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.XboxApp_48.62.6002.0_neutral_~_8wekyb3d8bbwe
+dism /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.XboxGameOverlay_1.49.24004.0_neutral_~_8wekyb3d8bbwe
+dism /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.XboxGamingOverlay_3.38.25003.0_neutral_~_8wekyb3d8bbwe
+dism /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.XboxIdentityProvider_12.58.1001.0_neutral_~_8wekyb3d8bbwe
+dism /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.XboxSpeechToTextOverlay_1.21.13002.0_neutral_~_8wekyb3d8bbwe
+dism /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.YourPhone_2020.305.2331.0_neutral_~_8wekyb3d8bbwe
+dism /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.ZuneMusic_2019.20022.11011.0_neutral_~_8wekyb3d8bbwe
+dism /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.ZuneVideo_2019.20022.11011.0_neutral_~_8wekyb3d8bbwe
+pause
